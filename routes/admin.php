14f8b3ab869d6e7c15847c2b59d78705ebed63db
Anil Kumar <anil.mauganj100@gmail.com>
2026-06-13 21:38:45 +0530

    Add admin category routes

---
+++ routes/admin.php
@@ -0,0 +1,10 @@
+<?php
+use App\Http\Controllers\Admin\CategoryController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        Route::resource('categories', CategoryController::class);
+    });
